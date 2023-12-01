c45626b11df46eb6485935de3baf1410d4348dea
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2023-12-01 18:16:18 +0100

    Add solution to day1 part1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::fs::read_to_string;
+
+fn main() -> Result<()> {
+    let input = read_to_string("input.txt")?;
+
+    // for every line extract numbers
+    let numbers: Vec<_> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .filter(|c| c.is_digit(10))
+                .collect::<Vec<char>>()
+        })
+        .collect();
+    // for every line find the first and last digit
+    let numbers: Vec<_> = numbers
+        .into_iter()
+        .map(|number| {
+            number.first().unwrap().to_digit(10).unwrap() * 10
+                + number.last().unwrap().to_digit(10).unwrap()
+        })
+        .collect();
+    // add them all up
+    let sum: u32 = numbers.iter().sum();
+
+    println!("Day 1 Part 1: {sum}");
+    Ok(())
 }
