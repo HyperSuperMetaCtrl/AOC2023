48f0ebf70d4ab12ca55fe1f597c22ad73830f2e8
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2023-12-03 23:47:31 +0100

    wip

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,9 +1,14 @@
 use anyhow::Result;
 use ndarray::{s, Array};
 use ndarray_ndimage::{pad, PadMode};
-use std::fs::read_to_string;
+use std::{fs::read_to_string, collections::HashMap};
 
 const DIM: usize = 140;
+type Point = (usize, usize);
+struct Screw {
+    touching: usize,
+    value: usize,
+}
 
 fn main() -> Result<()> {
     let mut sum = 0;
@@ -12,6 +17,7 @@ fn main() -> Result<()> {
     let mat = Array::from_shape_vec((DIM, DIM), input.into_bytes())?;
     let mat = pad(&mat, &[[1usize, 1usize]], PadMode::Constant(b'.'));
     let mut skip = 0;
+    let mut screws: HashMap<Point, Screw>= HashMap::new();
     for (index, elem) in mat.indexed_iter() {
         if skip > 0 {
             skip -= 1;
@@ -35,6 +41,9 @@ fn main() -> Result<()> {
             let count = frame
                 .iter()
                 .any(|b| !b.is_ascii_digit() && *b != b'.' && b.is_ascii_punctuation());
+            if let Some(screw) = frame
+                .iter()
+                .position(|b| *b == b'*') {todo!()}
             if count == true {
                 sum += num;
                 skip = offset
