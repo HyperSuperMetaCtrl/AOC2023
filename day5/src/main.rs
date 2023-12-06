dc0c2ea93c471e48cafdce0f65c7e32a21400623
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2023-12-06 18:10:51 +0100

    Improve perfoance: Day 5 Part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -8,7 +8,7 @@ use nom::{
     sequence::preceded,
     IResult,
 };
-use std::{fs::read_to_string, ops::Range, sync::{Arc, Mutex}};
+use std::fs::read_to_string;
 
 static STRINGS: [&str; 7] = [
     "seed-to-soil map:\n",
@@ -56,8 +56,8 @@ impl Almanac {
     fn transform(&self, mut input: u64) -> u64 {
         for map in &self.maps {
             if let Some(pos) = map
-                .par_iter()
-                .position_any(|x| (x[1]..(x[1] + x[2])).contains(&input))
+                .iter()
+                .position(|x| (x[1]..(x[1] + x[2])).contains(&input))
             {
                 let offset = input - map[pos][1];
                 input = map[pos][0] + offset;
@@ -98,26 +98,9 @@ fn main() -> Result<()> {
     let smallest = transformed.iter().min().unwrap();
     println!("Day 5 Part 1: {smallest}");
 
-    // let mut global_min: Arc<Mutex<Option<u64>>> = Arc::new(Mutex::new(None));
-    // let chunked_seeds: Vec<&[u64]> = almanac.seeds.par_chunks(2).collect();
-    // chunked_seeds.par_iter().for_each(|seeds| {
-    //     let mut range: Vec<u64> = (seeds[0]..(seeds[0] + seeds[1])).collect();
-    //     println!("transforming slice");
-    //     almanac.transform_slice(&mut range);
-    //     if let Some(min) = range.par_iter().min() {
-    //         let global_min = global_min.clone();
-    //         let mut global_min = global_min.lock().unwrap();
-    //         if  global_min.is_none() || min < &global_min.unwrap() {
-    //             println!("found min");
-    //             *global_min = Some(*min);
-    //         }
-    //     }
-    // });
-    // println!("Day 5 Part 2: {}", global_min.lock().unwrap().unwrap());
     let mut global_min: Option<u64> = None;
     for seeds in almanac.seeds.chunks(2) {
         let mut range: Vec<u64> = (seeds[0]..(seeds[0] + seeds[1])).collect();
-        dbg!(range.len());
         almanac.transform_slice(&mut range);
         if let Some(min) = range.par_iter().min() {
             if  global_min.is_none() || min < &global_min.unwrap() {
