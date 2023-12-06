3debb14f71694eedc5bc5eb69816fda72f252222
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2023-12-06 14:48:33 +0100

    Day 6 Part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,6 +4,7 @@ struct Race {
     time: usize,
     distance: usize,
 }
+
 impl Race {
     fn is_win(&self, button_time: usize) -> bool {
         let final_speed = ACC * button_time;
@@ -15,7 +16,24 @@ impl Race {
             false
         }
     }
+
+    fn number_of_wins(&self) -> usize {
+        let mut smallest = None;
+        let mut biggest = None;
+        for i in 0..=self.time {
+            if smallest.is_none() && self.is_win(i) {
+                smallest = Some(i);
+            }
+            if self.is_win(i) {
+                biggest = Some(i);
+            }
+        }
+        let smallest = smallest.unwrap();
+        let biggest = biggest.unwrap();
+        biggest-smallest+1
+    }
 }
+
 fn main() {
     let times = [40, 82, 84, 92];
     let distances = [233, 1011, 1110, 1487];
@@ -27,23 +45,15 @@ fn main() {
             distance: *distance,
         })
         .collect();
-    let mut wins: Vec<usize>= Vec::with_capacity(races.len());
+    let mut product = 1;
     for race in races {
-        let mut smallest = None;
-        let mut biggest = None;
-        for i in 0..=race.time {
-            if smallest.is_none() && race.is_win(i) {
-                smallest = Some(i);
-            }
-            if race.is_win(i) {
-                biggest = Some(i);
-            }
-        }
-        let smallest = smallest.unwrap();
-        let biggest = biggest.unwrap();
-        wins.push(biggest-smallest+1);
+        product *= race.number_of_wins();
     }
-    let product: usize = wins.iter().product();
     println!("Day 5 Part 1: {product}");
 
+    let race = Race {
+        time: 40828492,
+        distance: 233101111101487,
+    };
+    println!("Day 5 Part 2: {}", race.number_of_wins());
 }
