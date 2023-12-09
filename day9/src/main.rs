989e7e8066622dba5fb54b4cf15a73503623457c
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2023-12-09 16:03:25 +0100

    Day 9 Part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::read_to_string;
 
-fn read_input() -> Vec<Vec<i32>>{
+fn read_input() -> Vec<Vec<i32>> {
     let input = read_to_string("input.txt").unwrap();
     let lines: Vec<_> = input.lines().collect();
     let mut hists = Vec::with_capacity(lines.len());
@@ -15,20 +15,38 @@ fn read_input() -> Vec<Vec<i32>>{
     hists
 }
 
-fn predict(nums: &[i32]) -> i32 {
+fn diffs(nums: &[i32]) -> Vec<i32> {
     let mut diffs = vec![];
     for n in nums.windows(2) {
         let diff = n[1] - n[0];
         diffs.push(diff);
     }
+    diffs
+}
+enum Direction {
+    L,
+    R,
+}
+
+fn predict(nums: &[i32], direction: Direction) -> i32 {
+    let diffs = diffs(nums);
     if nums.iter().all(|x| *x == 0) {
         return 0;
-    } else {
-        return predict(&diffs) + nums.iter().last().unwrap();
+    }
+    match direction {
+        Direction::L => nums[0] - predict(&diffs, Direction::L),
+        Direction::R => predict(&diffs, Direction::R) + nums[nums.len() - 1],
     }
 }
+
 fn main() {
     let hists = read_input();
-    let sum: i32 = hists.iter().map(|x| predict(x)).sum();
-    println!("Day 9 Part 1: {sum}");
+    let predictions: Vec<(i32, i32)> = hists
+        .iter()
+        .map(|x| (predict(x, Direction::L), predict(x, Direction::R)))
+        .collect();
+    let (part2, part1): (i32, i32) = predictions
+        .iter()
+        .fold((0, 0), |acc, (x, y)| (acc.0 + x, acc.1 + y));
+    println!("Day 9 Part 1: {part1}, Part 2: {part2}");
 }
