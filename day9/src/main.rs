27cc7a1dc6d2c010fa5fab06ad8ddb98041325e9
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2023-12-09 15:32:08 +0100

    Add Day 9 Part 1

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,34 @@
+use std::fs::read_to_string;
+
+fn read_input() -> Vec<Vec<i32>>{
+    let input = read_to_string("input.txt").unwrap();
+    let lines: Vec<_> = input.lines().collect();
+    let mut hists = Vec::with_capacity(lines.len());
+    for line in lines {
+        let nums: Vec<_> = line.split(" ").collect();
+        let nums: Vec<_> = nums
+            .into_iter()
+            .map(|x| str::parse::<i32>(x).unwrap())
+            .collect();
+        hists.push(nums);
+    }
+    hists
+}
+
+fn predict(nums: &[i32]) -> i32 {
+    let mut diffs = vec![];
+    for n in nums.windows(2) {
+        let diff = n[1] - n[0];
+        diffs.push(diff);
+    }
+    if nums.iter().all(|x| *x == 0) {
+        return 0;
+    } else {
+        return predict(&diffs) + nums.iter().last().unwrap();
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let hists = read_input();
+    let sum: i32 = hists.iter().map(|x| predict(x)).sum();
+    println!("Day 9 Part 1: {sum}");
 }
